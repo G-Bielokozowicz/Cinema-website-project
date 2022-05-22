cac97076a626067697cdd77e8ecb574532cd0154
G-Blazkowicz <95439533+G-Blazkowicz@users.noreply.github.com>
2022-05-22 15:50:07 +0200

    Server init

---
+++ cinema2/backend/server.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const cors = require('cors')
+const mongoose=require('mongoose')
+
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(5000,()=>{
+    console.log('Server is running on port 5000')
+})
