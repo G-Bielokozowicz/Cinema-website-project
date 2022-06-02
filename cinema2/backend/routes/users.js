cb6fd082f50ae0fa8f67658ed61a76453596937f
G-Blazkowicz <95439533+G-Blazkowicz@users.noreply.github.com>
2022-06-02 14:54:31 +0200

    Added user registration and login authentication and password hashing

--- cinema2/backend/routes/users.js
+++ cinema2/backend/routes/users.js
@@ -15,26 +15,58 @@ router.route('/').get(async (req,res)=>{
 })
 
 // Register
-router.route('/add').post(async (req,res)=>{
+router.route('/add').post(asyncHandler (async (req,res)=>{
     const userEmail = req.body.userEmail
     const userPassword = req.body.userPassword
     const userType = req.body.userType
     
+    // Check if email and password are typed
+    if (!userEmail || !userPassword){
+        res.status(400)
+        throw new Error("Add all fields")
+    }
+
+    // Check if user exists
+    const userExists = await User.findOne({userEmail})
+    if (userExists){
+        res.status(400)
+        throw new Error("User already exists")
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(userPassword,salt)
+
+    // Create user
     const newUser = new User({
         userEmail,
-        userPassword,
+        userPassword: hashedPassword,
         userType,
     });
+
+    // Save user to database
     newUser.save()
     .then(() => res.json('User added'))
     .catch(err => res.status(400).json('Error: ' + err));
-})
+}))
 
+//TODO
 //Login
-router.route('/login').post(async (req,res)=>{
+router.route('/login').post(asyncHandler (async (req,res)=>{
+    const userEmail = req.body.userEmail
+    const userPassword = req.body.userPassword
 
-    res.json('User logged in')
+    // Find user with given email
+    const user = await User.findOne({userEmail})
 
-})
+    if (user && (await bcrypt.compare(userPassword, user.userPassword))){
+        res.json({
+            _id: user._id,
+            // email: user.userEmail,         
+        })
+    }
+
+    res.json('User not logged in')
+}))
 
 module.exports=router;
