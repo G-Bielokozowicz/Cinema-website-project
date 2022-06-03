7a8e0890ec79014c9b5262bcc22f17ec3a41c754
G-Blazkowicz <95439533+G-Blazkowicz@users.noreply.github.com>
2022-06-03 12:07:57 +0200

    Update Register.jsx

--- cinema2/client/src/components/Register.jsx
+++ cinema2/client/src/components/Register.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import {FaUser} from 'react-icons/fa'
 
 function Register() {
-  return (
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password: '',
+    })
+
+    const { name, email, password, password2} = formData
+
+    return (
     <div>Register</div>
-  )
+    )
 }
 
 export default Register
