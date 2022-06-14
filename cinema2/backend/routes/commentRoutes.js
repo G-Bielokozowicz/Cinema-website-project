62e1c17eed5ef87ea8611e4ef0099e1300118c80
G-Blazkowicz <95439533+G-Blazkowicz@users.noreply.github.com>
2022-06-14 13:36:57 +0200

    Added adding comment with checking if user has seen the movie

--- cinema2/backend/routes/commentRoutes.js
+++ cinema2/backend/routes/commentRoutes.js
@@ -2,11 +2,11 @@ const router = require('express').Router()
 let Comment = require('../models/comment.model')
 const { protect } = require('../middleware/authMiddleware')
 
-const {getCommentsByMovie,getCommentsByUser} = require('../controllers/commentController')
+const {getCommentsByMovie, getCommentsByUser, addComment} = require('../controllers/commentController')
 
 
 router.get('/movie/:movie', getCommentsByMovie)
-router.get('/user/:user',getCommentsByUser)
-//router.post('/add',protect,addMovie)
+router.get('/user',protect, getCommentsByUser)
+router.post('/add',protect,addComment)
 
 module.exports=router;
