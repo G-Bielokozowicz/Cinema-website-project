39f26b9e321bd410c666dc8c620efe59f67123a6
Wioletta Dziwińska <dziwinskawioletta@gmail.com>
2022-06-14 21:04:49 +0200

    centering register page

--- cinema2/client/src/components/Register.jsx
+++ cinema2/client/src/components/Register.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
 import {FaUser} from 'react-icons/fa'
+import styled from 'styled-components'
 
 function Register() {
 
@@ -25,68 +26,89 @@ function Register() {
     }
 
     return <>
-        <section className = "heading">
-            <h1>
-                <FaUser /> Register
-            </h1>
-            <p>Please create an account</p>
-        </section>
+        <WindowsStyle>
+            <section className = "heading">
+                <h2>
+                    <FaUser /> Register
+                </h2>
+                <p>Please create an account</p>
+            </section>
 
-        <section className='form'> 
-            <form onSubmit={onSubmit}>
-                <div className='form-group'>
-                    <input 
-                        type = "text" 
-                        className = 'form-control' 
-                        id='name'
-                        name='name' 
-                        value={name} 
-                        placeholder = 'Enter your name' 
-                        onChange={onChange}>
-                    </input>
-                </div>
-                <div>
-                    <input 
-                        type = "email" 
-                        className = 'form-control' 
-                        id='email'
-                        name='email' 
-                        value={email} 
-                        placeholder = 'Enter your email' 
-                        onChange={onChange}>
-                    </input>
-                </div>
-                <div>
-                    <input 
-                        type = "password" 
-                        className = 'form-control' 
-                        id='password'
-                        name='password' 
-                        value={password} 
-                        placeholder = 'Enter password' 
-                        onChange={onChange}>
-                    </input>
-                </div>
-                <div>
-                    <input 
-                        type = "password2" 
-                        className = 'form-control' 
-                        id='password2'
-                        name='password2' 
-                        value={password2} 
-                        placeholder = 'Confrim password' 
-                        onChange={onChange}>
-                    </input>
-                </div>
-                <div className='form-group'>
-                    <button type='submit' className='btn btn-block'>
-                        Submit
-                    </button>
-
-                </div>
-            </form>
-        </section>
+            <section className='form'> 
+                <form onSubmit={onSubmit}>
+                    <WindowStyle className='form-group'>
+                        <input 
+                            type = "text" 
+                            className = 'form-control' 
+                            id='name'
+                            name='name' 
+                            value={name} 
+                            placeholder = 'Enter your name' 
+                            size={30}
+                            onChange={onChange}>
+                        </input>
+                    </WindowStyle>
+                    <WindowStyle>
+                        <input 
+                            type = "email" 
+                            className = 'form-control' 
+                            id='email'
+                            name='email' 
+                            value={email} 
+                            placeholder = 'Enter your email' 
+                            size={30}
+                            onChange={onChange}>
+                        </input>
+                    </WindowStyle>
+                    <WindowStyle>
+                        <input 
+                            type = "password" 
+                            className = 'form-control' 
+                            id='password'
+                            name='password' 
+                            value={password} 
+                            placeholder = 'Enter password' 
+                            size={30}
+                            onChange={onChange}>
+                        </input>
+                    </WindowStyle>
+                    <WindowStyle>
+                        <input 
+                            type = "password2" 
+                            className = 'form-control' 
+                            id='password2'
+                            name='password2' 
+                            value={password2} 
+                            placeholder = 'Confrim password' 
+                            size={30}
+                            onChange={onChange}>
+                        </input>
+                    </WindowStyle>
+                    <ButtonStyle>
+                        <div className='form-group'>
+                            <button type='submit' className='btn btn-block'>
+                                Submit
+                            </button>
+                        </div>
+                    </ButtonStyle>
+                </form>
+            </section>
+        </WindowsStyle>
     </>
 }
+const WindowsStyle = styled.section`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+`;
+const WindowStyle = styled.div`
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+const ButtonStyle = styled.div`
+  display: grid;
+  justify-content: center;
+  //align-items: center;
+`;
 
 export default Register
