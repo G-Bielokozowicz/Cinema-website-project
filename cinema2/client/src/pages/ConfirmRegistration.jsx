24fca2b1b3953fce4bf4f3dba47318ad0125fd81
Wioletta Dziwińska <dziwinskawioletta@gmail.com>
2022-06-17 12:57:30 +0200

    Added wrpper to Confirm Registration Page

--- cinema2/client/src/pages/ConfirmRegistration.jsx
+++ cinema2/client/src/pages/ConfirmRegistration.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import Login from '../components/Login'
+import styled from 'styled-components'
+
 
 function ConfirmRegistration() {
   return (
     <div>
-        Rejestracja przebiegła pomyślnie.
-        Zaloguj się na swoje konto
+      <Wrapper2>
+        <Wrapper>
+          Rejestracja przebiegła pomyślnie.
+        </Wrapper>
+        <Wrapper>
+          Zaloguj się na swoje konto.
+        </Wrapper>
+      </Wrapper2>
         <Login/>
     </div>
   )
 }
+
+const Wrapper = styled.section`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Wrapper2 = styled.section`
+  margin-top: 30px;
+  margin-bottom: 10px;
+`;
 
 export default ConfirmRegistration
