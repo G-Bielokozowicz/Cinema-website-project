afdd845f1a463e378091f7018c2d7278b6094cf3
Wioletta Dziwińska <dziwinskawioletta@gmail.com>
2022-06-18 18:43:54 +0200

    Button leave comments

--- cinema2/client/src/components/MovieCard.jsx
+++ cinema2/client/src/components/MovieCard.jsx
@@ -11,14 +11,14 @@ function MovieCard(props) {
         <Card>
             <img src={props.image} width={200} height={300} alt='Poster'/>
             <Info>
-                <Title>
+                <Title to={`/movie/${props.name}`} state= {{ temp: [props.id, props.description, props.director, props.image] }}>
                     {props.name}
                 </Title>
                 <Description>
                     {props.description}
                 </Description>
-                    <MyLink to={`/movie/${props.name}`} state= {{ temp: [props.id, props.description, props.director, props.image] }}>
-                        Buy tickets
+                    <MyLink to={'comments'} state= {{ id: [props.id]}} >
+                        Leave comments
                     </MyLink>               
             </Info>
         </Card>
@@ -33,24 +33,31 @@ const Card = styled.section`
     border: 0;
     //margin-top:15px;
     //margin-left:20%;
-   // outline: red solid;
+    outline: red solid;
 `
 
 const Info = styled.div`
     display: flex;
     flex-wrap: wrap;
-    //outline: blue solid;
+    outline: blue solid;
     //margin-left: 10px;
     padding-left: 10px;
     max-width: 100%;
     grid-template-rows:80px;
     background-color: black;
 `
-const Title = styled.h1`
+const Title = styled(Link)`
+    background-color: black;
+    color: #ffffff;
     font-size: 1.25rem;
     font-weight: 600;
     margin: 2px;
     margin-bottom: 20px;
+    outline: green solid;
+    &:hover {
+    color: #8a8989;
+   // text-decoration: overline;
+    }
 `
 
 const Description = styled.p`
@@ -58,25 +65,26 @@ const Description = styled.p`
     font-weight: 600;
     margin: 2px;
     max-height: 60%;
+    outline: yellow solid;
 `
 
 const MyLink=styled(Link)`
     text-decoration: none;
     display: grid;
-    background-color: #d34d18;
-    color: #000;
-    height: 50px;
-    width: 150px;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: #000000;
+    color: #6a6a6a;
+    height: 40px;
+   // width: 190px;
     font-size: 20px;
     border-radius: 20px;
     cursor: pointer;
-    //outline: red solid;
+    outline: red solid;
     transition: color 0.4s;
     position: relative;
     left: auto;
     top: 20px;
-    justify-content: center;
-    align-items: center;
     &:hover {
     color: #ffffff;
     }
