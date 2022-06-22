073e06f45a0a20de94bab0c9f7b82c97e5c7639e
Wioletta Dziwińska <dziwinskawioletta@gmail.com>
2022-06-22 11:00:11 +0200

    The ability to add movies to the database from the admin panel

--- cinema2/client/src/components/AdminPanel.jsx
+++ cinema2/client/src/components/AdminPanel.jsx
@@ -18,11 +18,16 @@ function AdminPanel() {
 
     //console.log("dateTime: ", dateTime)
 
+    //dodwanie filmów  do bazy
+    const [name, setName] = useState('')
+    const [director, setDirector] = useState('')
+    let [length, setLength] = useState('')
+    let [description, setDescription] = useState('')
+    const [releaseYear, setReleaseYear] = useState('')
+    const [posterURL, setPosterURL] = useState('')
+
     //pobieraniee wszytskich filmw
     const [movies, setMovies] = useState([])
-
-
-    
 
     const getMovies = async () =>{
       axios.get('http://localhost:5000/movies/')
@@ -40,13 +45,12 @@ function AdminPanel() {
     },[])
 
     const [added, setAdded]= useState(false)
+    const [addedMovie, setAddedMovie]= useState(false)
     const [errored, setErrored]=useState(false)
     const [errMessage,setErrMessage]=useState('')
 
     //umieszczanie seansu w bazie
-    const token = JSON.parse(localStorage.getItem('user'))
-    //  console.log("token: " + token.token)
-      
+    const token = JSON.parse(localStorage.getItem('user'))    
     const config = {
         headers: { Authorization: `Bearer ${token.token}` }
     };
@@ -71,120 +75,272 @@ function AdminPanel() {
         })
     }
 
-  return (
-    <AddSreeningStyle>
-        <Headerstyle>
-            Add the screening values &nbsp;
-        </Headerstyle>
-        <TextStyle>
-            Choose movie name
-        </TextStyle>
-        <MovieStyle>
-            <select onChange={(e) => setMovieName(e.target.value)}>
-                {movies.map((movie)=>{
-                    return (
-                        <option value={movie._id} key={movie._id}>
-                            {movie.movieName}
-                        </option>
-                    )
-                })}
-            </select>
-        </MovieStyle>
-        <TextStyle>
-            Room number
-        </TextStyle>
-        <InputStyle
-            type='number'
-            name='room'
-            id='room'
-            value={room}
-            placeholder = 'Enter screening room' 
-            size="100"
-            min='0'
-            //height="30"
-            onChange={(e) => setRoom(e.target.value)}
-            >
-        </InputStyle> 
-        <TextStyle>
-            Screening date
-        </TextStyle>
-        <InputStyle
-            type='date'
-            name='date'
-            id='date'
-            value={date}
-            placeholder = 'Enter screening date' 
-            size="100"
-            //height="30"
-            onChange={(e) => setDate(e.target.value)}
-            >
-        </InputStyle> 
-        <TextStyle>
-            Screening time
-        </TextStyle>
-        <InputStyle
-            type='time'
-            name='time'
-            id='time'
-            value={time}
-            step = '2'
-            placeholder = 'Enter screening date' 
-            size="100"
-            //height="30"
-            onChange={(e) => setTime(e.target.value)}
-            >
-        </InputStyle>
-        <TextStyle>
-            Normal ticket price
-        </TextStyle> 
-        <InputStyle
-            type='number'
-            name='priceNormal'
-            id='priceNormal'
-            value={priceNormal}
-            placeholder = 'Enter ticket normal price' 
-            size="100"
-            min='12'
-            //height="30"
-            onChange={(e) => setPriceNormal(e.target.value)}
-            >
-        </InputStyle> 
-        <TextStyle>
-            Reduced ticket price
-        </TextStyle> 
-        <InputStyle
-            type='number'
-            name='priceReduced'
-            id='priceReduced'
-            value={priceReduced}
-            placeholder = 'Enter ticket reduced price' 
-            size="100"
-            min='8'
-            //height="30"
-            onChange={(e) => setPriceReduced(e.target.value)}
-            >
-        </InputStyle> 
+    const onSubmitMovie = (e) => {
+        e.preventDefault()
 
-        <form onSubmit = {onSubmit}>
-            <Button>
-                Add screening
-            </Button>
-        </form>
-        {
-            added ? (<>Screening added</>) : (<></>)
-        }
-        {
-            errored ? (<>{errMessage}</>):(<></>)
-        }
-    </AddSreeningStyle>
+        const data = {movieName: name, 
+                    movieDirector: director, 
+                    movieLength: length, 
+                    movieDescription: description, 
+                    movieReleaseYear: releaseYear,
+                    moviePosterURL: posterURL
+                }
+
+        axios.post('http://localhost:5000/movies/add', data, config)
+        .then(() => {
+            console.log("New screening added")
+            setAddedMovie(true)
+            setErrored(false)
+        })
+        .catch((error)=>{
+            setErrored(true)
+            setAddedMovie(false)
+            setErrMessage(error.response.data)
+            console.log(error.response.data);
+        })
+    }
+
+  return (
+    <Wrapper>
+        <AddSreeningStyle>
+            <Headerstyle>
+                Add the screening values &nbsp;
+            </Headerstyle>
+            <TextStyle>
+                Choose movie name
+            </TextStyle>
+            <MovieStyle>
+                <select onChange={(e) => setMovieName(e.target.value)}>
+                    {movies.map((movie)=>{
+                        return (
+                            <option value={movie._id} key={movie._id}>
+                                {movie.movieName}
+                            </option>
+                        )
+                    })}
+                </select>
+            </MovieStyle>
+            <TextStyle>
+                Room number
+            </TextStyle>
+            <InputStyle
+                type='number'
+                name='room'
+                id='room'
+                value={room}
+                placeholder = 'Enter screening room' 
+                size="100"
+                min='0'
+                //height="30"
+                onChange={(e) => setRoom(e.target.value)}
+                >
+            </InputStyle> 
+            <TextStyle>
+                Screening date
+            </TextStyle>
+            <InputStyle
+                type='date'
+                name='date'
+                id='date'
+                value={date}
+                placeholder = 'Enter screening date' 
+                size="100"
+                //height="30"
+                onChange={(e) => setDate(e.target.value)}
+                >
+            </InputStyle> 
+            <TextStyle>
+                Screening time
+            </TextStyle>
+            <InputStyle
+                type='time'
+                name='time'
+                id='time'
+                value={time}
+                step = '2'
+                placeholder = 'Enter screening date' 
+                size="100"
+                //height="30"
+                onChange={(e) => setTime(e.target.value)}
+                >
+            </InputStyle>
+            <TextStyle>
+                Normal ticket price
+            </TextStyle> 
+            <InputStyle
+                type='number'
+                name='priceNormal'
+                id='priceNormal'
+                value={priceNormal}
+                placeholder = 'Enter ticket normal price' 
+                size="100"
+                min='12'
+                //height="30"
+                onChange={(e) => setPriceNormal(e.target.value)}
+                >
+            </InputStyle> 
+            <TextStyle>
+                Reduced ticket price
+            </TextStyle> 
+            <InputStyle
+                type='number'
+                name='priceReduced'
+                id='priceReduced'
+                value={priceReduced}
+                placeholder = 'Enter ticket reduced price' 
+                size="100"
+                min='8'
+                //height="30"
+                onChange={(e) => setPriceReduced(e.target.value)}
+                >
+            </InputStyle> 
+
+            <form onSubmit = {onSubmit}>
+                <Button>
+                    Add screening
+                </Button>
+            </form>
+            <InfoStyle>
+                {
+                    added ? (<>Screening added</>) : (<></>)
+                }
+                {
+                    errored ? (<>{errMessage}</>):(<></>)
+                }
+            </InfoStyle>
+        </AddSreeningStyle>
+
+
+
+    {/* Dodawanie filmów */}
+        <AddSreeningStyle>
+            <Headerstyle>
+                Add the movies values &nbsp;
+            </Headerstyle>
+
+            <TextStyle>
+                Put movie name
+            </TextStyle>
+            <InputStyle
+                type='text'
+                name='name'
+                id='name'
+                value={name}
+                placeholder = 'Enter movie Name' 
+                size="100"
+                onChange={(e) => setName(e.target.value)}
+                >
+            </InputStyle> 
+
+            <TextStyle>
+                Movie director
+            </TextStyle>
+            <InputStyle
+                type='text'
+                name='director'
+                id='director'
+                value={director}
+                placeholder = 'Enter movie director' 
+                size="100"
+                onChange={(e) => setDirector(e.target.value)}
+                >
+            </InputStyle> 
+            <TextStyle>
+                Movie length
+            </TextStyle>
+            <InputStyle
+                type='number'
+                name='length'
+                id='length'
+                value={length}
+                placeholder = 'Enter movie length' 
+                size="100"
+                min={60}
+                onChange={(e) => setLength(e.target.value)}
+                >
+            </InputStyle> 
+
+            <TextStyle>
+                Movie description
+            </TextStyle>
+            <InputStyle
+                type='description'
+                name='description'
+                id='description'
+                value={description}
+                placeholder = 'Enter movie description' 
+                size="100"
+                onChange={(e) => setDescription(e.target.value)}
+                >
+            </InputStyle>
+            <TextStyle>
+                Year of the movie's release 
+            </TextStyle> 
+            <InputStyle
+                type='number'
+                name='releaseYear'
+                id='releaseYear'
+                value={releaseYear}
+                placeholder = 'Enter release year' 
+                size="100"
+                min='1930'
+                onChange={(e) => setReleaseYear(e.target.value)}
+                >
+            </InputStyle> 
+            <TextStyle>
+                Poster URL
+            </TextStyle> 
+            <InputStyle
+                type='url'
+                name='posterURL'
+                id='posterURL'
+                value={posterURL}
+                placeholder = 'Enter poster URL' 
+                size="100"
+                onChange={(e) => setPosterURL(e.target.value)}
+                >
+            </InputStyle> 
+
+            <form onSubmit = {onSubmitMovie}>
+                <Button>
+                    Add movie
+                </Button>
+            </form>
+            <InfoStyle>
+            {
+                addedMovie ? (<>Movie added</>) : (<></>)
+            }
+            {
+                errored ? (<>{errMessage}</>):(<></>)
+            }
+            </InfoStyle>    
+        </AddSreeningStyle>
+
+    </Wrapper>
+
   )
 }
+
+const InfoStyle = styled.section`
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 18px;
+ //   font-weight: initial;
+   // outline: yellow solid;
+    margin-left: 30%;
+    width: 40%;
+    margin-top: 3%;
+    margin-bottom: 1%;
+    color: #fffafa;
+`
 
 const TextStyle = styled.section`
     display: grid;
     font-size: 15px;
     font-weight: initial;
-  //  outline: yellow solid;
+   // outline: yellow solid;
     margin-left: 30%;
     width: 40%;
     margin-top: 3%;
@@ -202,7 +358,7 @@ const Headerstyle = styled.section`
 const MovieStyle = styled.section`
     display: grid;
     font-size: 15px;
-   // outline: blue solid;
+  //  outline: blue solid;
     margin-left: 30%;
     size: 10px;
     width: 40%;
@@ -215,7 +371,13 @@ const AddSreeningStyle = styled.section`
     justify-content: center;
     align-items: center;
   //  width: 50%;
-  //  outline: green solid;
+   // outline: green solid;
+`
+
+const Wrapper = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
 const InputStyle = styled.input`
@@ -232,7 +394,7 @@ const InputStyle = styled.input`
     width: 40%;
     /* height: 100%; */
     
-  //  outline: red solid;
+ //   outline: red solid;
 `
 
 const Button = styled.button`
