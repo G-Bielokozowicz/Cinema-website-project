4905c5adce8106c85034180b5baf8f0e7e3ca4de
G-Blazkowicz <95439533+G-Blazkowicz@users.noreply.github.com>
2022-11-23 19:41:59 +0100

    Moved client code to main folder

--- cinema2/server.js
+++ cinema2/server.js
@@ -30,7 +30,7 @@ app.use('/tickets',ticketRouter)
 app.use('/comments',commentRoutes)
 app.use('/movies',moviesRouter)
 
-const port = process.env.PORT || 5000;
+const port = process.env.SERVER_PORT || 5000;
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
